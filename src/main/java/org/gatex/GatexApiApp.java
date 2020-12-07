774c6566015b7ab11dbd03ab1ff9a0cf48d25ff0
Partha Sen <Partha_Sen@epam.com>
2020-12-07 16:11:27 +0530

    initial commit

---
+++ src/main/java/org/gatex/GatexApiApp.java
@@ -0,0 +1,12 @@
+package org.gatex;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatexApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GatexApiApp.class, args);
+    }
+}
