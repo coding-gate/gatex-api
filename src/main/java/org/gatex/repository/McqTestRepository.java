e275581bbea7c6ad6a567b8b51e27e225c3cf609
Partha Sen <Partha_Sen@epam.com>
2021-07-07 17:34:28 +0530

    assessment refactor

--- src/main/java/org/gatex/repository/McqTestRepository.java
+++ src/main/java/org/gatex/repository/McqTestRepository.java
@@ -1,8 +1,8 @@
 package org.gatex.repository;
 
-import org.gatex.entity.McqTest;
+import org.gatex.entity.Assessment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface McqTestRepository extends MongoRepository<McqTest, String>{
+public interface McqTestRepository extends MongoRepository<Assessment, String>{
 
 }
